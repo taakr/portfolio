847a2d0fc42680d268f8b320000cb1c02a389858
taakr <167289139+taakr@users.noreply.github.com>
2024-06-05 15:53:28 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// JavaScript
+//console.log('Hello!!!!');
+
+$(function(){
+    $('.bxslider').bxSlider({
+      mode: 'fade',
+      captions: true,
+      auto:true,
+      speed:500,
+      pause:2000,
+      autoHover:false,
+    });
+  });
